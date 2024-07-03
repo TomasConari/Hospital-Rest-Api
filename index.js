d1df2c1fb347d280d0239af3ba3aec43a4c8c148
Tomas Contreras <tomasconari@gmail.com>
2024-07-02 22:47:08 -0500

    Moved index.js

--- index.js
+++ index.js
@@ -1,19 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { conn } from "./database/db.js";
-import userRoutes from "./routes/user.routes.js"
-import productRoutes from "./routes/product.routes.js"
+import { conn } from "./src/database/db.js";
+import workerRoutes from "./src/routes/worker.routes.js";
+import medicineRoutes from "./src/routes/medicine.routes.js";
+import patientRoutes from "./src/routes/patient.routes.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 
-const PORT = 3000;
+const PORT = 8000;
 
-//routes
-app.use(userRoutes);
-app.use(productRoutes);
+app.use(workerRoutes);
+app.use(patientRoutes);
+app.use(medicineRoutes);
 
 conn();
 
